a7d9d69cb40dea9cad967c024c73117fe8839e2f
Alexander Lonsky <aleksandr.lonski.86@gmail.com>
2020-07-20 14:45:45 +0300

    create: slider for hotel

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+
+
+})
