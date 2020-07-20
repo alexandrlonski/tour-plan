9e4557d80743b8294ba9c0cea5fe68202a46f76d
Alexander Lonsky <aleksandr.lonski.86@gmail.com>
2020-07-20 14:47:46 +0300

    create:keyboard control for slider

--- js/main.js
+++ js/main.js
@@ -7,6 +7,15 @@ var mySwiper = new Swiper('.swiper-container', {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+  // Keyboard
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  speed: 800,
+  // grabCursor: true,
+  // simulateTouch: false,
 
 
 })
