933ade484abd1cd1ace62bb9db63754d1d8aab1b
Alexander Lonsky <aleksandr.lonski.86@gmail.com>
2020-07-30 22:30:17 +0300

    create: php fro two form

--- send.php
+++ send.php
@@ -4,11 +4,11 @@ require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
-// Переменные, которые отправляет пользователь
+if (isset($_POST['message'])) { // Переменные, которые отправляет пользователь
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-$email = $_POST['email'];
+
 
 
 // Формирование самого письма
@@ -17,8 +17,7 @@ $body = "
 <h2>Новое обращение</h2>
 <b>Имя:</b> $name<br>
 <b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br> $message <br>
-<b>Почта для рассылки:</b> $email 
+<b>Сообщение:</b><br> $message 
 ";
 
 // Настройки PHPMailer
@@ -33,7 +32,7 @@ try {
     // Настройки вашей почты
     $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
     $mail->Username   = 'forpractice.learning@gmail.com'; // Логин на почте
-    $mail->Password   = '1Ca2ojeb'; // Пароль на почте
+    $mail->Password   = ''; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
     $mail->setFrom('forpractice.learning@gmail.com', 'Александр Лонский'); // Адрес самой почты и имя отправителя
@@ -59,4 +58,60 @@ else {$result = "error";}
 }
 
 // Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status])
+// ;
+header('Location: thankyou.html'); }
+
+
+if (isset($_POST['email'])) { // Переменные, которые отправляет пользователь
+$email = $_POST['email'];
+
+
+// Формирование самого письма
+$title = "Новоя почта для рассылки newsletter Best Tour Plan";
+$body = "
+<b>Почта для рассылки:</b> $email 
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'forpractice.learning@gmail.com'; // Логин на почте
+    $mail->Password   = ''; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('forpractice.learning@gmail.com', 'Александр Лонский'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('aleks.lonya@yandex.ru');  
+   
+
+    // Прикрипление файлов к письму
+    
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status])
+// ;
+header('location: thankyouforyouremail.html');}
+
