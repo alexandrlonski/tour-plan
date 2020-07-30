84ac0327d07b1bf2096d8d17801ebf283e85cd8f
Alexander Lonsky <aleksandr.lonski.86@gmail.com>
2020-07-30 14:15:38 +0300

    fix: message height

--- send.php
+++ send.php
@@ -8,6 +8,7 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+$email = $_POST['email'];
 
 
 // Формирование самого письма
@@ -16,7 +17,8 @@ $body = "
 <h2>Новое обращение</h2>
 <b>Имя:</b> $name<br>
 <b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br> $message
+<b>Сообщение:</b><br> $message <br>
+<b>Почта для рассылки:</b> $email 
 ";
 
 // Настройки PHPMailer
