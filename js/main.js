f99ed789a25da4c6df4aaafce262934eb52c8c96
Alexander Lonsky <aleksandr.lonski.86@gmail.com>
2020-08-03 19:57:35 +0300

    added valitati0n for form & some fix

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
     // simulateTouch: false,
 
 
-  })
+  });
   var reviewsSlider = new Swiper('.reviews-slider', {
     // Optional parameters
     direction: 'horizontal',
@@ -40,7 +40,7 @@ $(document).ready(function () {
     // simulateTouch: false,
 
 
-  })
+  });
   ymaps.ready(init);
 
   function init() {
@@ -96,35 +96,32 @@ $(document).ready(function () {
     }
   });
 
-  // Отправка данных на сервер
-  // function send(event, php) {
-  //   console.log("Отправка запроса");
-  //   event.preventDefault ? event.preventDefault() : event.returnValue = false;
-  //   var req = new XMLHttpRequest();
-  //   req.open('POST', php, true);
-  //   req.onload = function () {
-  //     if (req.status >= 200 && req.status < 400) {
-  //       json = JSON.parse(this.response); // Ебанный internet explorer 11
-  //       console.log(json);
+  // Обработка форм
+  // $('.phone').mask('+7 (999) 999-99-99');
+  $('.phone').mask('+7(999)999-99-99', {
+    'translation': {
+      9: {
+        pattern: /[0-9*]/
+      }
+    }
+  });
+  $(".form").each(function () {
+    $(this).validate({
 
-  //       // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
-  //       if (json.result == "success") {
-  //         // Если сообщение отправлено
-  //         alert("Сообщение отправлено");
-  //       } else {
-  //         // Если произошла ошибка
-  //         alert("Ошибка. Сообщение не отправлено");
-  //       }
-  //       // Если не удалось связаться с php файлом
-  //     } else {
-  //       alert("Ошибка сервера. Номер: " + req.status);
-  //     }
-  //   };
-
-  //   // Если не удалось отправить запрос. Стоит блок на хостинге
-  //   req.onerror = function () {
-  //     alert("Ошибка отправки запроса");
-  //   };
-  //   req.send(new FormData(event.target));
-  // }
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlenght: "Name must be at least 3 letters"
+        },
+        email: {
+          required: "We need add your email",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: {
+          required: " Phone is required",
+        },
+      },
+    });
+  });
 });
