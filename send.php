845f5c6ebc51c9f62b3e279f2fffadaa6ca1066e
Alexander Lonsky <aleksandr.lonski.86@gmail.com>
2020-08-03 12:44:38 +0300

    added new letter for form booking

--- send.php
+++ send.php
@@ -4,7 +4,10 @@ require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
-if (isset($_POST['message'])) { // Переменные, которые отправляет пользователь
+// ----    send-message -------
+
+if (isset($_POST['send-message'])) { 
+    // Переменные, которые отправляет пользователь
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
@@ -60,17 +63,74 @@ else {$result = "error";}
 // Отображение результата
 // echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status])
 // ;
-header('Location: thankyou.html'); }
+header('Location: send-message.html'); }
 
+// ----    subscription -------
 
-if (isset($_POST['email'])) { // Переменные, которые отправляет пользователь
+if (isset($_POST['subscription'])) {
+     // Переменные, которые отправляет пользователь
 $email = $_POST['email'];
-
-
 // Формирование самого письма
 $title = "Новоя почта для рассылки newsletter Best Tour Plan";
 $body = "
 <b>Почта для рассылки:</b> $email 
+";
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'forpractice.learning@gmail.com'; // Логин на почте
+    $mail->Password   = ''; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('forpractice.learning@gmail.com', 'Александр Лонский'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('aleks.lonya@yandex.ru');  
+  // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status])
+// ;
+header('location: subscription.html');}
+
+// ----    booking ------
+
+if (isset($_POST['booking'])) { 
+    // Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+
+// Формирование самого письма
+$title = "Booking Best Tour Plan";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br> $message <br>
+<b>Почта:</b> $email 
 ";
 
 // Настройки PHPMailer
@@ -92,11 +152,7 @@ try {
 
     // Получатель письма
     $mail->addAddress('aleks.lonya@yandex.ru');  
-   
-
-    // Прикрипление файлов к письму
-    
-    // Отправка сообщения
+   // Отправка сообщения
     $mail->isHTML(true);
     $mail->Subject = $title;
     $mail->Body = $body;    
@@ -113,5 +169,5 @@ else {$result = "error";}
 // Отображение результата
 // echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status])
 // ;
-header('location: thankyouforyouremail.html');}
+header('Location: booking.html'); }
 
